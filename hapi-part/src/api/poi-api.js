c6c451034a35e88189ab403cbc5a89439776c4c2
David <david.trieu@st.oth-regensburg.de>
2023-06-22 12:29:12 +0200

    added maps

--- hapi-part/src/api/poi-api.js
+++ hapi-part/src/api/poi-api.js
@@ -26,9 +26,7 @@ export const poiApi = {
     },
 
     findOne: {
-        auth: {
-            strategy: "jwt",
-        },
+        auth: false,
         handler: async function(request, h) {
             try {
                 const poi = await db.poiStore.getPOIById(request.params.id);
@@ -44,7 +42,7 @@ export const poiApi = {
         description: "Get one point of interest",
         notes: "Returns one point of interest",
         validate: { params: { id: IdSpec }, failAction: validationError },
-        response: { schema: POISpecPlusLocation, failAction: validationError },
+        //response: { schema: POISpecPlusLocation, failAction: validationError },
     },
 
     create: {
